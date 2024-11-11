70620a2f098245b08c9a82115e5c6471478559e8
Tim Wallace <timwallacedev@gmail.com>
2024-11-11 15:30:38 -0500

    move logic from binary file into as ts file to allow for testing

--- bin/iabc.js
+++ bin/iabc.js
@@ -1,21 +1,8 @@
 #!/usr/bin/env node
-import make from '../src/cli/make.js';
-import create from '../src/cli/curl-route-builder.js';
+import handleCommand from "../src/cli/handle-command.js"
 
-// Retrieve command-line arguments
-const args = process.argv.slice(2);
-const command = args[0];
-
-if (command === undefined) {
-    make(process.argv, process.cwd(),);
+try {
+    handleCommand(process.argv, process.cwd())
+}catch(error){
+    console.error(error)
 }
-else {
-    if (command === "curl" || command === "har" || command === "open-api"){
-        create(process.argv, process.cwd(),);
-    }
-    else {
-        throw new Error("unknown command: " + command);
-    }
-}
-
-
